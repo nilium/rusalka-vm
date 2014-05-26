e9919347d78d3532cd592634ac01f248b3ea0a54
Noel Cower <ncower@gmail.com>
2014-05-26 01:17:44 -0700

    Update vm_test.cpp
    Does nothing really new. Just loads code and tries to run it through
    the VM.

--- vm_test.cpp
+++ vm_test.cpp
@@ -23,20 +23,26 @@
 
 
 value_t printfn(vm_state_t &vm, int32_t argc, const value_t *argv) {
-  std::cout << "PRINT: ";
+  std::cerr << "PRINTI: ";
   for (; argc > 0; --argc, ++argv) {
-    std::cout << *argv << ' ';
+    std::cerr << argv->i32() << ' ';
   }
-  std::cout << std::endl;
+  std::cerr << std::endl;
   return value_t { 0 };
 }
 
 value_t printsfn(vm_state_t &vm, int32_t argc, const value_t *argv) {
-  std::cout << "PRINT: ";
+  std::cerr << "PRINTS: ";
   for (; argc > 0; --argc, ++argv) {
-    std::cout << (const char *)vm.get_block(argv->i32(), VM_MEM_READABLE) << ' ';
+    const char *ptr = reinterpret_cast<const char *>(vm.get_block(argv->i32(), VM_MEM_READABLE));
+    int32_t size = vm.block_size(*argv);
+    if (!ptr || !size) {
+      std::cerr << "<null> ";
+      continue;
+    }
+    std::cerr << std::string(ptr, size) << ' ';
   }
-  std::cout << std::endl;
+  std::cerr << std::endl;
   return value_t { 0 };
 }
 
@@ -52,31 +58,30 @@ int main(int argc, char const *argv[])
 {
   vm_unit_t unit;
 
+  std::cerr << "Unit is valid: " << unit.is_valid() << std::endl;
+
   {
-    std::cerr << "Opening stream." << std::endl;
+    std::cerr << "Opening stream (test.asm.bc x1)." << std::endl;
     std::fstream stream ("test.asm.bc", std::ios_base::in);
     std::cerr << "Reading bytecode." << std::endl;
     unit.read(stream);
   }
 
-  {
-    std::cerr << "Opening stream." << std::endl;
-    std::fstream stream ("test.asm.bc", std::ios_base::in);
-    std::cerr << "Reading bytecode." << std::endl;
-    unit.read(stream);
-  }
+  unit.debug_write_instructions(std::cerr);
 
-  // vm_state_t vm(2048);
-  // vm.set_source(source_t(std::ifstream("out.bc")));
-  // vm.bind_callback("print", printfn);
-  // vm.bind_callback("prints", printsfn);
-  // float fv = vm.function("main")(2.010101);
-  // std::clog << "Returned: " << fv << std::endl;
+  std::cerr << "Unit is valid: " << unit.is_valid() << std::endl;
 
-  // #ifdef LOG_FINAL_STATE
-  // vm.dump_registers();
-  // vm.dump_stack();
-  // #endif
+  vm_state_t vm;
+  vm.set_unit(std::move(unit));
+  vm.bind_callback("print", printfn);
+  vm.bind_callback("prints", printsfn);
+  double fv = vm.function("__main__")(-123.456);
+  std::clog << "Returned: " << fv << std::endl;
+
+  #ifdef LOG_FINAL_STATE
+  vm.dump_registers();
+  vm.dump_stack();
+  #endif
 
   return 0;
 }
