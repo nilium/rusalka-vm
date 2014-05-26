218cf3468e706b46a1c419f6326679ab4d821cc8
Noel Cower <ncower@gmail.com>
2014-05-26 01:14:59 -0700

    Simplify test.asm a bit for testing.

--- test.asm
+++ test.asm
@@ -21,46 +21,35 @@ defdata woop "wooperton"
 
 // main
 .main:
-    push      $0
-    load      $1 0.5
-    push      $1
     call      .fabs 1
-
-    add       $0 $0 rp
-
+    add       $0 rp 123.456
+    push      $0
+    call      ^print 1
     push      $0
     call      ^print 1
 
-    load      $20 ~woop
-    push      $20
+    load      $2 ~woop
+    push      $2
     call      ^prints 1
 
-    load      $20 "woop"
-    push      $20
+    load      $2 "woop"
+    push      $2
     call      ^prints 1
 
-    push      $0
-    load      $0 0
-
-    call      .extern_sym1 0
-
-    load      $8 .extern_sym2
-    call      $8 0
-
-@loop:
-    for $0 < 2048 {
+    for $0 < 256 {
       add     $0 $0 1.0
+      push    $0
+      call    ^print 1
     }
 
-    pop       $0
+    load      rp $0
     return
 
 // fabs(f). Just import a freakin' fabs function, this is stupid since I can't
 // just bitwise-and a double's sign bit since integers are strictly 32-bit
 // and therefore cannot touch the latter 32 bits of a double.
 .fabs:
-    load      rp $0
+    pop       rp
     if rp < 0.0
       neg     rp rp
-@fabs_return_early:
     return
