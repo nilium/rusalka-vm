5f0990fb657e0da63c768a49bc5b468380dad2cb
Noel Cower <ncower@gmail.com>
2014-05-26 16:25:06 -0700

    Apparently this file never got added.. weird.
    Okay, well, it's there now.

---
+++ vm_op.cpp
@@ -0,0 +1,15 @@
+#include "vm_op.h"
+#include "vm_unit.h"
+
+
+opcode_t op_t::opcode() const
+{
+  return unit.instructions[ip].opcode;
+}
+
+
+value_t op_t::operator [] (int32_t index) const
+{
+  int32_t argv_base = unit.instructions[ip].arg_pointer;
+  return unit.instruction_argv[argv_base + index];
+}
