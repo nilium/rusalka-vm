14f33f08c41a599cbdb54cf78f50c189b11f645a
Noel Cower <ncower@gmail.com>
2014-06-07 23:36:27 -0700

    License header formatting.

--- vm_op.cpp
+++ vm_op.cpp
@@ -1,9 +1,10 @@
 /*
-  Copyright Noel Cower 2014.
-  Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE_1_0.txt or copy at
-  http://www.boost.org/LICENSE_1_0.txt)
-*/
+ *          Copyright Noel Cower 2014.
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
 
 #include "vm_op.h"
 #include "vm_unit.h"
