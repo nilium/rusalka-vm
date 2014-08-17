77eaf2010eeeedcd48db6e7abbadb8182881faa9
Noel Cower <ncower@gmail.com>
2014-08-16 23:52:53 -0700

    Split rusalka into librusalka and vm_test code.
    Currently produces a static library. Haven't tested how it works with
    a shared library since it doesn't provide a way to supply an allocator
    or anything, and I consider that kind of important for shared
    libraries.

--- premake4.lua
+++ premake4.lua
@@ -1,12 +1,44 @@
 solution 'rusalka'
 configurations {'Debug', 'Release'}
 
+project 'rusalka'
+do
+  kind 'StaticLib'
+  language 'C++'
+  flags { 'ExtraWarnings' }
+
+  files { '**.h', '**.inl', '**.cpp' }
+  excludes { '**_test.cpp', '**_test.h', '**_test.inl' }
+
+  flags { 'ExtraWarnings' }
+
+  configuration 'macosx'
+  do
+    buildoptions { '-std=gnu++11', '-stdlib=libc++' }
+    links { 'c++' }
+  end
+
+  configuration 'Debug'
+  do
+    defines { 'DEBUG' }
+    flags { 'Symbols' }
+  end
+
+  configuration 'Release'
+  do
+    defines { 'NDEBUG' }
+    flags { 'Optimize' }
+  end
+end
+
+
 project 'vm_test'
 do
   kind 'ConsoleApp'
   language 'C++'
 
-  files { '**.h', '**.inl', '**.cpp' }
+  files { 'vm_test.cpp' }
+  links { 'rusalka' }
 
   flags { 'ExtraWarnings' }
 
