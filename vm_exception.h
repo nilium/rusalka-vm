8e513522931c13e3f4c76d2942fee948200169f7
Noel Cower <ncower@gmail.com>
2014-11-10 19:07:30 -0800

    Reorganize exceptions header.
    Make it slightly easier to pick up doc comments for exceptions.

--- vm_exception.h
+++ vm_exception.h
@@ -12,6 +12,44 @@
 #include <stdexcept>
 
 
+struct vm_runtime_error;
+struct vm_logic_error;
+/** Thrown if bytecode attempts to access a register that doesn't exist. */
+struct vm_bad_register;
+/** Exception thrown for jumping to or calling an invalid instruction pointer. */
+struct vm_invalid_instruction_pointer;
+/** Thrown if pop is called when ebp == esp */
+struct vm_stack_underflow;
+/** Thrown if accessing a stack index < 0. */
+struct vm_stack_access_error;
+/** Used when peek/poke/memmove something outside the bounds of a block. */
+struct vm_memory_access_error;
+/** Thrown if the permissions on a memory block don't satisfy the access */
+/** requested for a block. */
+struct vm_memory_permission_error;
+/**
+ * Exception specifically used when there's an attempt to peek, poke, memdup,
+ * or memmove the zero block or any other operation not permitted on the zero
+ * block.
+ */
+struct vm_null_access_error;
+/** Exception thrown when a CALL instruction has an argc < 0 or > esp. */
+struct vm_invalid_argument_count;
+/** Exception thrown for using a thread with the wrong process. */
+struct vm_wrong_process;
+/** Thrown if bytecode contains an unrecognized opcode. */
+struct vm_bad_opcode;
+/** Generic unit loading consistency error */
+struct vm_bad_unit;
+/** Generic unit loading IO error */
+struct vm_unit_io_error;
+/**
+ * Exception thrown specifically for units that are of an unsupported
+ * bytecode version.
+ */
+struct vm_unsupported_unit_version;
+
+
 #define VM_DECLARE_EXCEPTION(KLASSNAME, SUPERKLASSNAME) \
   struct KLASSNAME : public SUPERKLASSNAME { \
     KLASSNAME (char const *what_arg) : SUPERKLASSNAME(what_arg) { /* nop */ } \
@@ -23,37 +61,20 @@
 VM_DECLARE_EXCEPTION(vm_runtime_error, std::runtime_error);
 VM_DECLARE_EXCEPTION(vm_logic_error, std::logic_error);
 
-
-// Thrown if bytecode attempts to access a register that doesn't exist.
 VM_DECLARE_EXCEPTION(vm_bad_register, vm_logic_error);
-// Exception thrown for jumping to or calling an invalid instruction pointer.
 VM_DECLARE_EXCEPTION(vm_invalid_instruction_pointer, vm_logic_error);
-// Thrown if pop is called when ebp == esp
 VM_DECLARE_EXCEPTION(vm_stack_underflow, vm_logic_error);
-// Thrown if accessing a stack index < 0.
 VM_DECLARE_EXCEPTION(vm_stack_access_error, vm_logic_error);
-// Used when peek/poke/memmove something outside the bounds of a block.
 VM_DECLARE_EXCEPTION(vm_memory_access_error, vm_logic_error);
-// Thrown if the permissions on a memory block don't satisfy the access
-// requested for a block.
 VM_DECLARE_EXCEPTION(vm_memory_permission_error, vm_memory_access_error);
-// Exception specifically used when there's an attempt to
-// peek/poke/memdup/memmove block zero.
 VM_DECLARE_EXCEPTION(vm_null_access_error, vm_memory_access_error);
-// Exception thrown when a CALL instruction has an argc < 0 or > esp.
 VM_DECLARE_EXCEPTION(vm_invalid_argument_count, vm_logic_error);
-// Exception thrown for using a thread with the wrong process.
 VM_DECLARE_EXCEPTION(vm_wrong_process, vm_logic_error);
 
-
-// Thrown if bytecode contains an unrecognized opcode.
 VM_DECLARE_EXCEPTION(vm_bad_opcode, vm_runtime_error);
-// Generic unit loading consistency error
-VM_DECLARE_EXCEPTION(vm_bad_unit, vm_runtime_error);
-// Generic unit loading IO error
 VM_DECLARE_EXCEPTION(vm_unit_io_error, vm_runtime_error);
-// Exception thrown specifically for units that are of an unsupported
-// bytecode version.
+VM_DECLARE_EXCEPTION(vm_bad_unit, vm_runtime_error);
+
 VM_DECLARE_EXCEPTION(vm_unsupported_unit_version, vm_bad_unit);
 
 #undef VM_DECLARE_EXCEPTION
