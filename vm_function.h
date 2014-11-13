49a4de5ff2f14da42ce8aae23e8b64060f22035f
Noel Cower <ncower@gmail.com>
2014-11-12 19:47:48 -0800

    Document vm_function.h

--- vm_function.h
+++ vm_function.h
@@ -17,13 +17,28 @@
 class vm_thread;
 
 
+// Semi-internal.
 template <class... ARGS>
 vm_value vm_invoke_function(vm_thread &thread, int64_t pointer, ARGS &&... args);
 
 
+/**
+ * Invokable function type for Rusalka.
+ *
+ * vm_function is bound to a specific thread and, given an instruction pointer,
+ * can be used to call a VM function (whether it's a host callback or bytecode
+ * function) similar to a normal function. Automagically converts its arguments
+ * to VM values.
+ */
 class vm_function
 {
+  /** The thread the function is bound to. */
   vm_thread &_thread;
+  /**
+   * The instruction pointer of the function.
+   *
+   * If negative, points to a host callback.
+   */
   int64_t _pointer;
 
   vm_function(vm_thread &thread, int64_t pointer)
@@ -36,11 +51,21 @@ class vm_function
   friend vm_thread;
 
 public:
+  /** Copy ctor. */
   vm_function(const vm_function &other) = default;
+
   vm_function() = delete;
 
+  /**
+   * Call operator. Given any number of arguments, will attempt to convert them
+   * all to VM value types and call the function with those after pushing them
+   * onto the stack.
+   */
   template <class... ARGS>
   vm_value operator()(ARGS&&... args);
+  /**
+   * Argumentless call operator. Does what you think it does.
+   */
   vm_value operator() ();
 };
 
