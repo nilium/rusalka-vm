55ec804561c8699bfc7fefe268176a311d235096
Noel Cower <ncower@gmail.com>
2014-11-15 01:49:00 -0800

    Document vm_op.h
    Too tired to do anything else, so this is my daily routine now:
    documenting my VM headers.

--- vm_op.h
+++ vm_op.h
@@ -17,13 +17,24 @@
 class vm_unit;
 
 
+/**
+ * A VM op.
+ *
+ * Acts as a wrapper interface for a particular VM unit and an instruction
+ * pointer to allow access to that instruction's opcode and operands.
+ */
 class vm_op
 {
 public:
+  /** The VM unit the op is bound to. */
   vm_unit const &unit;
+  /** The instruction pointer into the VM unit. */
   int64_t const ip;
 
+  /** The litflag mask for the op. */
   uint64_t litflag() const;
+  /** The op's opcode. */
   vm_opcode opcode() const;
+  /** Subscript operator to access the op's operands / arguments. */
   vm_value operator [] (int64_t index) const;
 };
